9a226e51a3b7de8efe8bcb96fa0703464ca18ac1
Vineeth Reddy Kancharakuntla <eternalbasic1@gmail.com>
2023-02-24 11:57:44 +0530

    Updated Backend CORS

--- app.js
+++ app.js
@@ -33,28 +33,44 @@ mongoose.connect(process.env.DATABASE,{
 app.use(bodyParser.json());//it Parse incoming request(/ , /admin , /cart  ) bodies in a middleware before your handlers, available under the req.body property. In req.body property we have everything like req.body.name ,req.body.email , req.body.password etc..
 app.use(cookieParser());// Handles the header and populate req.cookie. If we want to set something into the cookie or if we want to get something form the cookie we use cookie-parser.
 
-app.use(cors());  // Cross-Origin Resource Sharing (CORS) is a security feature implemented by web browsers that blocks web pages from making requests to a different domain than the one that served the web page. This is done to prevent malicious websites from making requests to your web browser on behalf of the user.
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    next();
-  });
+// app.use(cors());  // Cross-Origin Resource Sharing (CORS) is a security feature implemented by web browsers that blocks web pages from making requests to a different domain than the one that served the web page. This is done to prevent malicious websites from making requests to your web browser on behalf of the user.
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "*");
+//     next();
+//   });
+// var corsOptions = {
+//     origin: 'https://mern-tshirt-app.onrender.com/',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   }
+
+// // My Routes // Prefixing every route using /api for good practice , in future if you want to edit  route names you can direct come here and edit it which will be reflected to all routes
+// app.use("/api", cors(corsOptions),(req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "*");
+//   next();
+// },authRoutes);
+
+// app.use("/api",userRoutes);
+// app.use("/api",categoryRoutes);
+// app.use("/api",productRoutes);
+// app.use("/api",orderRoutes);
+// app.use("/api",stripeRoutes);
+// // app.use("/api",paymentBRoutes);
+
+// TODO: If you wanted to do something in production with cors policy do it in below format
 var corsOptions = {
-    origin: 'https://mern-tshirt-app.onrender.com/',
+    origin: 'https://mernystore.onrender.com',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 
 // My Routes // Prefixing every route using /api for good practice , in future if you want to edit  route names you can direct come here and edit it which will be reflected to all routes
-app.use("/api", cors(corsOptions),(req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  next();
-},authRoutes);
-app.use("/api",userRoutes);
-app.use("/api",categoryRoutes);
-app.use("/api",productRoutes);
-app.use("/api",orderRoutes);
-app.use("/api",stripeRoutes);
+app.use("/api",cors(corsOptions), authRoutes);
+app.use("/api",cors(corsOptions), userRoutes);
+app.use("/api",cors(corsOptions), categoryRoutes);
+app.use("/api",cors(corsOptions), productRoutes);
+app.use("/api",cors(corsOptions), orderRoutes);
+app.use("/api",cors(corsOptions), stripeRoutes);
 // app.use("/api",paymentBRoutes);
 
 
@@ -64,13 +80,13 @@ const port = process.env.PORT||8000;
 
 if( process.env.NODE_ENV == "production"){
 
-    app.use(express.static("client/build"));
+    app.use(express.static("build"));
 
     const path = require("path");
 
     app.get("*", (req, res) => {
 
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 
     })
 
